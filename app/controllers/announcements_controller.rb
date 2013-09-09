b8363660a7482000c50325bb20d8878d4ad1042f
kassimhalloyta <kassimhalloyta@hotmail.com>
2013-09-09 19:05:09 +0200

    Update announcements_controller.rb

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,16 +1,15 @@
 class AnnouncementsController < ApplicationController
   def new
   	@announcement = Announcement.new
-
   end
+  
   def create
   @announcement = Announcement.new(params[:announcement])
   if @announcement.save
-  	
-
     redirect_to root_path, :notice => "announcement is successfully created !!!"
   else
     render :new
   end
-end
+  end
+
 end
